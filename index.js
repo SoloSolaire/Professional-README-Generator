46f38d5eb11fc891ed8e82ca7a9de450983dfbd2
SoloSolaire <128697724+SoloSolaire@users.noreply.github.com>
2023-06-05 04:29:19 -0700

    added the 'requires' and prompt questions

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What is the title of your project?',
+            name: 'title'
+        },
+        {
+            type: 'input',
+            message: 'Please add a description to your README.',
+            name: 'description'
+        },
+        {
+            type: 'input',
+            message: 'Please add installation instructions.',
+            name: 'install'
+        },
+        {
+            type: 'input',
+            message: 'Please add usage information.',
+            name: 'usage'
+        },
+        {
+            type: 'input',
+            message: 'Please add contribution guidelines.',
+            name: 'guideline'
+        },
+        {
+            type: 'input',
+            message: 'Please add test instructions.',
+            name: 'test'
+        }
+    ])
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
